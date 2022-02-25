4f64f9b6145e53e7bae1e39f980507cda24e34b1
xterikatx <--global>
2022-02-25 08:56:10 -0300

    add catogorylist styles

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Button } from "../../components/Forms/Button";
+import { CategorySelect } from "../../components/Forms/CategorySelect";
 import { Input } from "../../components/Forms/Input";
 import { TransactionTypeButton } from "../../components/Forms/TransactionTypeButton";
 
@@ -42,6 +43,8 @@ export function Register() {
               onPress={() => handleTransactionTypeSelect("down")}
             />
           </TransactionTypes>
+
+          <CategorySelect title="Categoria" />
         </Fields>
 
         <Button title="Enviar" />
