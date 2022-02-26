9d95ab9b9e5871506a3f77e61f077620cd45e3a3
xterikatx <--global>
2022-02-26 17:50:33 -0300

    add modal and list categories

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
+import { Modal } from "react-native";
 import { Button } from "../../components/Forms/Button";
-import { CategorySelect } from "../../components/Forms/CategorySelect";
+import { CategorySelectButton } from "../../components/Forms/CategorySelectButton";
 import { Input } from "../../components/Forms/Input";
 import { TransactionTypeButton } from "../../components/Forms/TransactionTypeButton";
+import { CategorySelect } from "../CategorySelect";
 
 import {
   Container,
@@ -15,9 +17,22 @@ import {
 
 export function Register() {
   const [transactionType, setTransactionType] = useState("");
+  const [categoryModal, setCategoryModal] = useState(false);
+  const [category, setCategory] = useState({
+    key: "category",
+    name: "Categoria",
+  });
 
   function handleTransactionTypeSelect(type: "up" | "down") {
     setTransactionType(type);
+  }
+
+  function handleOpenCategorySelectModal() {
+    setCategoryModal(true);
+  }
+
+  function handleCloseCategorySelectModal() {
+    setCategoryModal(false);
   }
 
   return (
@@ -44,11 +59,18 @@ export function Register() {
             />
           </TransactionTypes>
 
-          <CategorySelect title="Categoria" />
+          <CategorySelectButton title={category.name} onPress={handleOpenCategorySelectModal} />
         </Fields>
 
-        <Button title="Enviar" />
+        <Button title="Selecinar" />
       </Form>
+      <Modal visible={categoryModal}>
+        <CategorySelect
+          category={category}
+          setCategory={setCategory}
+          closeSelectCategory={handleCloseCategorySelectModal}
+        />
+      </Modal>
     </Container>
   );
 }
